43b75cc76f78b6af510f021f1686b28216faf2b0
Christoffer G. Thomsen <chris@cgt.name>
2016-11-11 21:56:32 +0100

    Add test for invalid MAIL FROM
    Invalid because of superfluous space.

--- smtpd_test.go
+++ smtpd_test.go
@@ -115,3 +115,65 @@ func TestRejectRecipient(t *testing.T) {
 	c.Close()
 	cancel()
 }
+
+func TestInvalidMailFromSpace(t *testing.T) {
+	logger := log.New(ioutil.Discard, "", 0)
+	serverHostname := "server.invalid"
+	s := Server{
+		Addr:     "127.0.0.1:0",
+		Hostname: serverHostname,
+		OnMailFrom: func(c Connection, from MailAddress) error {
+			return nil
+		},
+		Log: logger,
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	ln, err := s.Listen()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("listening on %s", ln.Addr())
+	go func() {
+		err = s.Serve(ctx, ln)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	c, err := textproto.Dial("tcp", ln.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.Close()
+
+	code, msg, err := c.ReadCodeLine(220)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	id, err := c.Cmd("HELO client.invalid")
+	if err != nil {
+		t.Fatal(err)
+	}
+	c.StartResponse(id)
+	code, msg, err = c.ReadResponse(250)
+	if err != nil {
+		t.Fatal(err)
+	}
+	c.EndResponse(id)
+
+	id, err = c.Cmd("MAIL FROM: <superfluous.space@example.net")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	c.StartResponse(id)
+	code, msg, err = c.ReadResponse(501)
+	if err != nil {
+		t.Fatalf("expected response code 501, got %v: %v", code, msg)
+	}
+
+	cancel()
+}
